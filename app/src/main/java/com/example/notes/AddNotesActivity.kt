7d70f18989a99fc2cadd3aebbc201a6159b35daa
user <it22271082@my.sliit.lk>
2024-06-26 11:38:55 +0530

    save2

--- app/src/main/java/com/example/notes/AddNotesActivity.kt
+++ app/src/main/java/com/example/notes/AddNotesActivity.kt
@@ -1,6 +1,5 @@
 package com.example.notes
 
-
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,12 +18,21 @@ class AddNotesActivity : AppCompatActivity() {
         db = NotesDataBaseHelper(this)
 
         binding.saveButton.setOnClickListener {
-            val title = binding.titleEditText.text.toString()
-            val content = binding.contentEditText.text.toString()
-            val note = Note(0, title, content)
-            db.insertNote(note)
-            finish()
-            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+            val title = binding.titleEditText.text.toString().trim()
+            val content = binding.contentEditText.text.toString().trim()
+
+            if (title.isEmpty() || content.isEmpty()) {
+                Toast.makeText(this, "Title and Content cannot be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                val note = Note(
+                    id = 0,
+                    title = title,
+                    comment = content
+                )
+                db.insertNote(note)
+                finish()
+                Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
